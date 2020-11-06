c2660c3333205ef39d1b11703513fc8cf712dc55
Miller <nmiller2379@gmail.com>
2020-11-05 20:37:36 -0700

    updated api to fix live-site problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,29 +35,29 @@ $(document).ready(function () {
 })
 
 $("#redoButton").on("click", function () {
-    var quotesAPI = "https://quotes.stormconsultancy.co.uk/random.json"
+    var quotesAPI = "https://programming-quotes-api.herokuapp.com/quotes/random"
     fetch(quotesAPI)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            $("#quoteText").text(data.quote) 
-            quote = data.quote;
-            console.log("test" + quote)
+            $("#quoteText").text(data.en) 
+            quote = data.en;
+            console.log("test" + en)
         })
 })
 
 $(document).ready(function () {
-    var quotesAPI = "https://quotes.stormconsultancy.co.uk/random.json"
+    var quotesAPI = "https://programming-quotes-api.herokuapp.com/quotes/random"
     fetch(quotesAPI)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            $("#quoteText").text(data.quote)
-            quote = data.quote;
+            $("#quoteText").text(data.en)
+            quote = data.en;
         })
 })
 
